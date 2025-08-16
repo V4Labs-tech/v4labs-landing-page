de5eafa5b6ade5ec97759129fea476be527c10be
arijit <arijitstoic@gmail.com>
2025-08-16 21:05:20 +0530

    Updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+ import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
+ if (process.env.NODE_ENV === 'development') {
+   await setupDevPlatform();
+ }
 const nextConfig: NextConfig = {
   /* config options here */
 };
+
+export const runtime = "edge";
 
 export default nextConfig;
